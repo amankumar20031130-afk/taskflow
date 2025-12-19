6aa98ddd115720719f9b2afd241462ccb3502459
Aman Kumar <amankumar20031130@gmail.com>
2025-12-20 00:48:54 +0530

    Configure frontend for production

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_BACKEND_URL || "";
+const URL = import.meta.env.VITE_BACKEND_URL || "https://taskflow-2r5g.onrender.com";
 
 export const socket = io(URL, {
     withCredentials: true,
